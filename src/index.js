b9b86bb45e72abcbcb2a1e458ce490f65f7d67bc
sofiia-ch <chubasofiia@gmail.com>
2023-06-20 11:28:06 +0300

    Moved the existing project to the folder in the GItHub repository

---
+++ src/index.js
@@ -0,0 +1,48 @@
+let now = new Date();
+
+let hours = now.getHours();
+let minutes = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let timing = document.querySelector("#today");
+timing.innerHTML = `${day} ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+
+  let city = document.querySelector("#city");
+  city.innerHTML = `${searchInput.value}`;
+}
+let form = document.querySelector("#search-form");
+
+form.addEventListener("submit", search);
+
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  console.log(temperature);
+  let temperatureToday = document.querySelector("#temperature-today");
+  temperatureToday.innerHTML = `${temperature}`;
+}
+
+
+let apiKey = "f81614abe2395d5dfecd45b9298041de";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(showTemperature);
+
+
+//let city = document.querySelector("#search-text-input");
+
